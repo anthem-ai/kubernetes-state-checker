c47e33dbac418701aab10ad8cd08fade1058025c
gar <garlandk@gmail.com>
2020-11-20 01:25:48 +0000

    Looking up the service to check the ports
    Signed-off-by: gar <garlandk@gmail.com>

--- src/checks/kubernetes/services/ports/ports.go
+++ src/checks/kubernetes/services/ports/ports.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"fmt"
 	"reflect"
-	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -50,40 +48,34 @@ func (i inputs) DoesPortExist() Results {
 	fmt.Println("Running :" + i.checkName)
 	fmt.Println("Checking port: " + i.checkValues["port"])
 
+	// Set initial check results
+	checkResult := Results{
+		DidPass: false,
+		Message: "Port not found",
+	}
+
 	// Run kube stuff
-	i.clientSet.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-
-	for {
-
-		pods, err := i.clientSet.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = i.clientSet.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
-
-		time.Sleep(10 * time.Second)
+	services, err := i.clientSet.CoreV1().Services(i.namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
 	}
 
-	testResult := Results{
-		DidPass: true,
-		Message: "Port found",
+	for _, aService := range services.Items {
+
+		if aService.ObjectMeta.Name == i.checkValues["serviceName"] {
+			fmt.Println("Found service: " + aService.ObjectMeta.Name)
+
+			for _, port := range aService.Spec.Ports {
+				fmt.Println(fmt.Sprint(port.Port) + " : " + string(i.checkValues["port"]))
+				if fmt.Sprint(port.Port) == string(i.checkValues["port"]) {
+					fmt.Println("Found port: " + i.checkValues["port"])
+
+					checkResult.DidPass = true
+					checkResult.Message = "Port found"
+				}
+			}
+		}
 	}
-	return testResult
+
+	return checkResult
 }
