eda7e88873b5cc38a0d4d1bf15e81ce822bb59a8
sekka1 <garlandk@gmail.com>
2020-12-11 11:11:24 -0800

    Pod check (#11)
    * Adding pod check for current state
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding unit tests
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Fixing pod order check status and adding unit tests to cover those use cases
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Enabling the checks in the config
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Removing commented out code
    
    Signed-off-by: gar <garlandk@gmail.com>

---
+++ src/checks/kubernetes/pods/pods.go
@@ -0,0 +1,121 @@
+package pods
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"regexp"
+
+	"gopkg.in/yaml.v2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type inputs struct {
+	valuesYaml string
+	checkName  string
+	namespace  string
+}
+
+type Results struct {
+	DidPass bool
+	Message string
+}
+
+// New New
+func New(valuesYaml string, checkName string, namespace string) inputs {
+	s := inputs{valuesYaml, checkName, namespace}
+
+	return s
+}
+
+type podStruct struct {
+	Values struct {
+		ChecksEnabled struct {
+			State []struct {
+				PodName      string `yaml:"podName"`
+				DesiredState string `yaml:"desiredState"`
+			} `yaml:"state,omitempty"`
+		} `yaml:"checksEnabled"`
+	} `yaml:"values"`
+}
+
+func podParse(valuesYaml string, v *podStruct) error {
+
+	err := yaml.Unmarshal([]byte(valuesYaml), &v)
+	if err != nil {
+		return errors.New(fmt.Sprintf("YAML Parse Error: %v", err))
+	}
+
+	return nil
+}
+
+// GeneralCheck GeneralCheck
+func (i inputs) GeneralCheck(kubeClientSet kubernetes.Interface) Results {
+
+	var values podStruct
+
+	// Set initial check results
+	checkResult := Results{
+		DidPass: false,
+		Message: "",
+	}
+
+	didValuesParse := false
+
+	err := podParse(i.valuesYaml, &values)
+	if err != nil {
+		didValuesParse = true
+		checkResult.Message = fmt.Sprintf("%v", err)
+	}
+
+	if !didValuesParse {
+
+		// Get data from kubernetes
+		pods, err := kubeClientSet.CoreV1().Pods(i.namespace).List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		//
+		// Check pod state
+		//
+		didFindError := false
+		for _, inputPod := range values.Values.ChecksEnabled.State {
+
+			didFindContainer := false
+
+			for _, aPod := range pods.Items {
+
+				// Match string for the input pod
+				match, _ := regexp.MatchString(inputPod.PodName, aPod.ObjectMeta.Name)
+
+				if match {
+
+					didFindContainer = true
+
+					if inputPod.DesiredState == string(aPod.Status.Phase) {
+						checkResult.DidPass = true
+						checkResult.Message += "* Pod " + aPod.ObjectMeta.Name + " is in " + inputPod.DesiredState + " state\n"
+					} else {
+						checkResult.Message += "* Pod " + aPod.ObjectMeta.Name + " is NOT in " + inputPod.DesiredState + " state\n"
+						didFindError = true
+					}
+				}
+			}
+
+			if !didFindContainer {
+				checkResult.Message += "* Did not find pod: " + inputPod.PodName + "\n"
+				didFindError = true
+			}
+		}
+
+		// Found one or more errors
+		if didFindError {
+			checkResult.DidPass = false
+		}
+
+	}
+
+	return checkResult
+}
---
+++ src/checks/kubernetes/pods/pods_test.go
@@ -0,0 +1,320 @@
+package pods
+
+import (
+	"reflect"
+	"testing"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestNew(t *testing.T) {
+	type args struct {
+		valuesYaml string
+		checkName  string
+		namespace  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want inputs
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(tt.args.valuesYaml, tt.args.checkName, tt.args.namespace); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_podParse(t *testing.T) {
+	type args struct {
+		valuesYaml string
+		v          *podStruct
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := podParse(tt.args.valuesYaml, tt.args.v); (err != nil) != tt.wantErr {
+				t.Errorf("podParse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_inputs_GeneralCheck(t *testing.T) {
+	type fields struct {
+		valuesYaml string
+		checkName  string
+		namespace  string
+	}
+	type args struct {
+		kubeClientSet kubernetes.Interface
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   Results
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Checking pod state - 2 container (positive)",
+			fields: fields{
+				checkName: "check1",
+				namespace: "ns1",
+				valuesYaml: `---
+values:
+  checksEnabled:
+    state:
+    - podName: pod-1-
+      desiredState: Running`,
+			},
+			args: args{
+				// Doc/example: https://gianarb.it/blog/unit-testing-kubernetes-client-in-go
+				kubeClientSet: fake.NewSimpleClientset(&v1.PodList{
+					Items: []v1.Pod{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:        "pod-1-123abc-abc",
+								Namespace:   "ns1",
+								Annotations: map[string]string{},
+							},
+							Status: v1.PodStatus{
+								Phase: "Running",
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:        "pod-2222222-123abc-abc",
+								Namespace:   "ns1",
+								Annotations: map[string]string{},
+							},
+							Status: v1.PodStatus{
+								Phase: "Running",
+							},
+						},
+					},
+				}),
+			},
+			want: Results{
+				DidPass: true,
+				Message: "* Pod pod-1-123abc-abc is in Running state\n",
+			},
+		},
+		{
+			name: "Checking pod state - 1 container (positive)",
+			fields: fields{
+				checkName: "check1",
+				namespace: "ns1",
+				valuesYaml: `---
+values:
+  checksEnabled:
+    state:
+    - podName: pod-1-
+      desiredState: Running`,
+			},
+			args: args{
+				// Doc/example: https://gianarb.it/blog/unit-testing-kubernetes-client-in-go
+				kubeClientSet: fake.NewSimpleClientset(&v1.PodList{
+					Items: []v1.Pod{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:        "pod-1-123abc-abc",
+								Namespace:   "ns1",
+								Annotations: map[string]string{},
+							},
+							Status: v1.PodStatus{
+								Phase: "Running",
+							},
+						},
+					},
+				}),
+			},
+			want: Results{
+				DidPass: true,
+				Message: "* Pod pod-1-123abc-abc is in Running state\n",
+			},
+		},
+		{
+			name: "Checking pod state - no containers found (negative)",
+			fields: fields{
+				checkName: "check1",
+				namespace: "ns1",
+				valuesYaml: `---
+values:
+  checksEnabled:
+    state:
+    - podName: pod-1-
+      desiredState: Running`,
+			},
+			args: args{
+				// Doc/example: https://gianarb.it/blog/unit-testing-kubernetes-client-in-go
+				kubeClientSet: fake.NewSimpleClientset(&v1.PodList{
+					Items: []v1.Pod{},
+				}),
+			},
+			want: Results{
+				DidPass: false,
+				Message: "* Did not find pod: pod-1-\n",
+			},
+		},
+		{
+			name: "Checking pod state - found one container but not another (negative)",
+			fields: fields{
+				checkName: "check1",
+				namespace: "ns1",
+				valuesYaml: `---
+values:
+  checksEnabled:
+    state:
+    - podName: pod-1-
+      desiredState: Running
+    - podName: pod-2-
+      desiredState: Running`,
+			},
+			args: args{
+				// Doc/example: https://gianarb.it/blog/unit-testing-kubernetes-client-in-go
+				kubeClientSet: fake.NewSimpleClientset(&v1.PodList{
+					Items: []v1.Pod{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:        "pod-1-123abc-abc",
+								Namespace:   "ns1",
+								Annotations: map[string]string{},
+							},
+							Status: v1.PodStatus{
+								Phase: "Running",
+							},
+						},
+					},
+				}),
+			},
+			want: Results{
+				DidPass: false,
+				Message: `* Pod pod-1-123abc-abc is in Running state
+* Did not find pod: pod-2-
+`,
+			},
+		},
+		{
+			name: "Checking pod state - pod-2 in an incorrect state (negative)",
+			fields: fields{
+				checkName: "check1",
+				namespace: "ns1",
+				valuesYaml: `---
+values:
+  checksEnabled:
+    state:
+    - podName: pod-1-
+      desiredState: Running
+    - podName: pod-2-
+      desiredState: Running`,
+			},
+			args: args{
+				// Doc/example: https://gianarb.it/blog/unit-testing-kubernetes-client-in-go
+				kubeClientSet: fake.NewSimpleClientset(&v1.PodList{
+					Items: []v1.Pod{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:        "pod-1-123abc-abc",
+								Namespace:   "ns1",
+								Annotations: map[string]string{},
+							},
+							Status: v1.PodStatus{
+								Phase: "Running",
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:        "pod-2-123abc-abc",
+								Namespace:   "ns1",
+								Annotations: map[string]string{},
+							},
+							Status: v1.PodStatus{
+								Phase: "ContainerCreating",
+							},
+						},
+					},
+				}),
+			},
+			want: Results{
+				DidPass: false,
+				Message: `* Pod pod-1-123abc-abc is in Running state
+* Pod pod-2-123abc-abc is NOT in Running state
+`,
+			},
+		},
+		{
+			name: "Checking pod state - pod-2 in an incorrect state - reverse pod order (negative)",
+			fields: fields{
+				checkName: "check1",
+				namespace: "ns1",
+				valuesYaml: `---
+values:
+  checksEnabled:
+    state:
+    - podName: pod-2-
+      desiredState: Running
+    - podName: pod-1-
+      desiredState: Running`,
+			},
+			args: args{
+				// Doc/example: https://gianarb.it/blog/unit-testing-kubernetes-client-in-go
+				kubeClientSet: fake.NewSimpleClientset(&v1.PodList{
+					Items: []v1.Pod{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:        "pod-1-123abc-abc",
+								Namespace:   "ns1",
+								Annotations: map[string]string{},
+							},
+							Status: v1.PodStatus{
+								Phase: "Running",
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:        "pod-2-123abc-abc",
+								Namespace:   "ns1",
+								Annotations: map[string]string{},
+							},
+							Status: v1.PodStatus{
+								Phase: "ContainerCreating",
+							},
+						},
+					},
+				}),
+			},
+			want: Results{
+				DidPass: false,
+				Message: `* Pod pod-2-123abc-abc is NOT in Running state
+* Pod pod-1-123abc-abc is in Running state
+`,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			i := inputs{
+				valuesYaml: tt.fields.valuesYaml,
+				checkName:  tt.fields.checkName,
+				namespace:  tt.fields.namespace,
+			}
+			if got := i.GeneralCheck(tt.args.kubeClientSet); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("inputs.GeneralCheck() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
