24c93a8b9d605e18933efe62a372e38d9c6ccd17
gar <garlandk@gmail.com>
2021-01-22 23:42:53 +0000

    Adding build.sh script for the release
    Signed-off-by: gar <garlandk@gmail.com>

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Modified from: https://github.com/ngs/go-release.action/blob/master/build.sh
+
+set -eux
+
+PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
+
+# mkdir -p $PROJECT_ROOT
+# rmdir $PROJECT_ROOT
+# ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
+# cd $PROJECT_ROOT
+go get -v ./...
+
+EXT=''
+
+if [ $GOOS == 'windows' ]; then
+EXT='.exe'
+fi
+
+if [ -x "./build.sh" ]; then
+  OUTPUT=`./build.sh "${CMD_PATH}"`
+else
+  go build "${CMD_PATH}"
+  OUTPUT="${PROJECT_NAME}${EXT}"
+fi
+
+echo ${OUTPUT}
